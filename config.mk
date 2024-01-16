a2e9e8761f3f0257154c46b676158c5650ff91be
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2024-01-16 15:57:26 +0100

    Add conditional branch on overflow

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@
 # RV32IMA := opcodes-rv32i opcodes-rv32m opcodes-rv32a opcodes-system
 
 # Xpulpimg
-RV32XPULPIMG := opcodes-xpulpabs_CUSTOM opcodes-xpulpbr_CUSTOM opcodes-xpulpclip_CUSTOM opcodes-xpulpmacsi_CUSTOM opcodes-xpulpminmax_CUSTOM opcodes-xpulpslet_CUSTOM opcodes-xpulpvect_CUSTOM opcodes-xpulpvectshufflepack_CUSTOM
+RV32XPULPIMG := opcodes-xpulpabs_CUSTOM opcodes-xpulpclip_CUSTOM opcodes-xpulpmacsi_CUSTOM opcodes-xpulpminmax_CUSTOM opcodes-xpulpslet_CUSTOM opcodes-xpulpvect_CUSTOM opcodes-xpulpvectshufflepack_CUSTOM
 RV32XPULPIMG += opcodes-xpulpbitop_CUSTOM
 # RV32XPULPIMG += opcodes-xpulpbitopsmall_CUSTOM #is a subset of opcodes-xpulpbitop_CUSTOM
 # RV32XPULPIMG += opcodes-xpulppostmod_CUSTOM #conflict with opcodes-ssr_CUSTOM
@@ -12,5 +12,5 @@ RV32XPULPIMG += opcodes-xpulpbitop_CUSTOM
 SNITCH_OPCODES := opcodes-dma_CUSTOM opcodes-frep_CUSTOM opcodes-ssr_CUSTOM opcodes-sflt_CUSTOM
 
 # default configurations
-MEMPOOL_ISA := $(RV32XPULPIMG) opcodes-xpulppostmod_CUSTOM opcodes-rv32d-zfh_DRAFT opcodes-rv32q-zfh_DRAFT opcodes-rv32zfh_DRAFT opcodes-rv64zfh_DRAFT opcodes-sflt_CUSTOM opcodes-xqueue_CUSTOM
+MEMPOOL_ISA := $(RV32XPULPIMG) opcodes-xpulppostmod_CUSTOM opcodes-rv32d-zfh_DRAFT opcodes-rv32q-zfh_DRAFT opcodes-rv32zfh_DRAFT opcodes-rv64zfh_DRAFT opcodes-sflt_CUSTOM opcodes-xqueue_CUSTOM opcodes-xovbr_CUSTOM
 SNITCH_ISA := $(RV32XPULPIMG) $(SNITCH_OPCODES)
