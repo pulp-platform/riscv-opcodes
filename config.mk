6c3ada988630947e960382ccd1613fbad68f1886
sem24f2 Lucia Luzi (luzil) <sem24f2@badile23.ee.ethz.ch>
2024-03-26 16:44:12 +0100

    added vfshuffle opcode

--- config.mk
+++ config.mk
@@ -13,4 +13,4 @@ SNITCH_OPCODES := opcodes-dma_CUSTOM opcodes-frep_CUSTOM opcodes-ssr_CUSTOM
 
 # default configurations
 MEMPOOL_ISA := opcodes-frep_CUSTOM $(RV32XPULPIMG) opcodes-xpulppostmod_CUSTOM opcodes-rv32d-zfh_DRAFT opcodes-rv32q-zfh_DRAFT opcodes-rv32zfh_DRAFT opcodes-rv64zfh_DRAFT opcodes-sflt_CUSTOM
-SNITCH_ISA := $(RV32XPULPIMG) $(SNITCH_OPCODES) opcodes-sflt_CUSTOM
+SNITCH_ISA := $(RV32XPULPIMG) $(SNITCH_OPCODES) opcodes-sflt_CUSTOM opcodes-shuffling
